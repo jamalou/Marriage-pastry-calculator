6167cfe92b635b61bcff980fe33f8c2ab6643c5e
Mohamed JAMEL EDDINE <mohamedelhasni.jameleddine@gmail.com>
2024-05-09 03:58:43 +0000

    improved add item

--- server/src/orderItems.js
+++ server/src/orderItems.js
@@ -16,25 +16,14 @@ async function fetchProductDetails(productId) {
 
 // Add an item to an order
 async function addItemToOrder(orderId, itemData) {
-    const orderRef = db.collection('orders').doc(orderId);
-
-    const itemRef = orderRef.collection('items').doc(); // Create a new document for the item
     const productDetails = await fetchProductDetails(itemData.product_id);
-    
     // Check if productDetails and required properties are available and valid
-    if (
-        !productDetails
-        || isNaN(parseInt(productDetails.product_piece_per_kilo))
-        || isNaN(parseFloat(productDetails.product_price))
-    ) {
-        console.error('Invalid product details');
-        if (!productDetails) {
-            throw new Error('Product not found');
-        } else if (isNaN(parseInt(productDetails.product_piece_per_kilo))) {
-            throw new Error('Invalid pieces per kilo');
-        } else if (isNaN(parseFloat(productDetails.product_price))) {
-            throw new Error('Invalid price');
-        }
+    if (!productDetails) {
+        throw new Error('Product not found');
+    } else if (isNaN(parseInt(productDetails.product_piece_per_kilo))) {
+        throw new Error('Invalid pieces per kilo');
+    } else if (isNaN(parseFloat(productDetails.product_price))) {
+        throw new Error('Invalid price');
     }
 
     itemData.product_piece_per_kilo = productDetails.product_piece_per_kilo;
@@ -72,12 +61,48 @@ async function addItemToOrder(orderId, itemData) {
         console.error('Weight calculation failed');
         throw new Error('Invalid weight calculation');
     }
+    const orderRef = db.collection('orders').doc(orderId);
+    const itemsRef = orderRef.collection('items');
+    // Start a transaction to ensure data consistency
+    const { updatedOrder, itemId } = await db.runTransaction(async (transaction) => {
+        const orderDoc = await transaction.get(orderRef);
+        const itemsSnapshot = await transaction.get(itemsRef);
+        const orderData = orderDoc.data();
+        const orderItems = []
+        let newTotalPrice = 0;
+        let newTotalWeight = 0.0;
+        let newNbPieces = 0;
 
-    await itemRef.set(itemData);
-    // await computeOrderGlobals(orderId);
-    const updatedOrder = await getOrderById(orderId);
+        itemsSnapshot.forEach(doc => {
+            newTotalPrice += doc.data().total_price;
+            newTotalWeight += doc.data().total_weight;
+            newNbPieces += doc.data().total_number_pieces;
+            orderItems.push({ id: doc.id, ...doc.data() })
+        });
+        newTotalPrice += itemData.total_price;
+        newTotalWeight += itemData.total_weight;
+        newNbPieces += itemData.total_number_pieces;
+        const newItemRef = orderRef.collection('items').doc();
+        transaction.set(newItemRef, itemData);
+        // 4. Update the total price in the order document
+        transaction.update(orderRef, {
+            order_total_price: newTotalPrice,
+            order_total_weight: newTotalWeight,
+            order_total_number_pieces: newNbPieces
+        });
+        orderItems.push({id:newItemRef.id, ...itemData})
+        const updatedOrder = {
+            id: orderId,
+            ...orderData,
+            order_total_price: newTotalPrice,
+            order_total_weight: newTotalWeight,
+            order_total_number_pieces: newNbPieces,
+            order_items: orderItems
+        };
+        return {itemId: newItemRef.id, updatedOrder: updatedOrder}
+    });
 
-    return {itemId: itemRef.id, updatedOrder: updatedOrder};
+    return {itemId: itemId, updatedOrder: updatedOrder};
 }
 
 // Update an item in an order
@@ -141,6 +166,8 @@ async function updateItemInOrder(orderId, itemId, updateData) {
         throw error; // Re-throw to handle it in the calling context if necessary
     }
 }
+
+
 
 // Delete an item from an order
 async function deleteItemFromOrder(orderId, itemId) {
